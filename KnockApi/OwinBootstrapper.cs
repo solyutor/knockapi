60fb93e579320ddaab0399e884f70862d7d67b48
Jury Soldatenkov <yurysold@googlemail.com>
2015-06-18 23:56:02 +0300

    Initial commit

---
+++ KnockApi/OwinBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace KnockApi
+{
+    public class OwinBootstrapper
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWebApi(new HttpConfiguration());
+        }
+    }
+}
---
+++ KnockApi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace KnockApi
+{
+    internal class Program
+    {
+        private static IDisposable _owinHost;
+
+        private static void Main(string[] args)
+        {
+            _owinHost = WebApp.Start<OwinBootstrapper>("http://*:80");
+            Console.ReadKey();
+            _owinHost.Dispose();
+        }
+    }
+}
