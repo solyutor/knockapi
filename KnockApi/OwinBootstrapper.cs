3e5494a8adbd09ed6eb7c60e4090b12deffd076d
Jury Soldatenkov <yurysold@googlemail.com>
2015-06-19 00:05:28 +0300

    Installed knockout and static files middleware

--- KnockApi/OwinBootstrapper.cs
+++ KnockApi/OwinBootstrapper.cs
@@ -7,6 +7,7 @@ namespace KnockApi
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseStaticFiles();
             app.UseWebApi(new HttpConfiguration());
         }
     }
